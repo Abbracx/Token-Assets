9f0431ea68dfdd673ab5695e330e987e0a0f678f
Abbracx <tankoraphael@gmail.com>
2022-02-25 18:37:19 +0100

    Added migrations for TokenSale smart contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 const DecagonToken = artifacts.require("DecagonToken");
-
+const TokenSale = artifacts.require("TokenSale")
 module.exports = async function(deployer) {
-  const token = await deployer.deploy(DecagonToken, 10000);
+
+  const addresses = web3.eth.getAccounts();
+  await deployer.deploy(DecagonToken, 10000);
+  await deployer.deploy(TokenSale, 1, addresses[0], DecagonToken.address)
+  //Next take care of sending money from addresses[0] to TokenSale address
+  //Token Sale needs to own the contract to destribute
+  token = await DecagonToken.deployed()
+  await token.transfer(TokenSale.address, 10000)
 };
